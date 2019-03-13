2be08a777db785f9bc2245e28147fd6d077d4250
NewAzgardec <46683133+NewAzgardec@users.noreply.github.com>
2019-03-13 17:36:25 +0300

    Added 2 part

---
+++ MyService.java
@@ -0,0 +1,34 @@
+package com.example.service;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+public class MyService extends IntentService {
+
+    public MyService()
+    {
+        super(MyService.class.getSimpleName());
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+
+        sendMyBroadCast();
+    }
+
+    private void sendMyBroadCast()
+    {
+        try
+        {
+            Intent broadCastIntent = new Intent();
+            broadCastIntent.setAction(MainActivity.BROADCAST_ACTION);
+
+            sendBroadcast(broadCastIntent);
+
+        }
+        catch (Exception ex)
+        {
+            ex.printStackTrace();
+        }
+    }
+}
