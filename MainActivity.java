00ed85c16f56cde62db861590f877dd8cc1a6b31
Masha <masha13242@gmail.com>
2019-03-14 04:13:54 +0300

    Init fields in onCreate

--- MainActivity.java
+++ MainActivity.java
@@ -1,69 +1,49 @@
 package com.example.xiaomi;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
     TextView displayResultPlaceView;
     EditText displayFirstNumPlaceView;
     EditText displaySecondNumPlaceView;
+
+    int num1 = Integer.parseInt(displayFirstNumPlaceView.getText().toString());
+    int num2 = Integer.parseInt(displaySecondNumPlaceView.getText().toString());
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        displayFirstNumPlaceView = findViewById(R.id.firstNumber);
+        displaySecondNumPlaceView = findViewById(R.id.secondNumber);
+        displayResultPlaceView= findViewById(R.id.result);
+
     }
     
 
     public void sumOfElements (View v){
-        displayFirstNumPlaceView = findViewById(R.id.firstNumber);
-        displaySecondNumPlaceView = findViewById(R.id.secondNumber);
-        displayResultPlaceView= findViewById(R.id.result);
-
-        int num1 = Integer.parseInt(displayFirstNumPlaceView.getText().toString());
-        int num2 = Integer.parseInt(displaySecondNumPlaceView.getText().toString());
         int res = num1 + num2;
-
         displayResultPlaceView.setText(Integer.toString(res));
-
     }
 
     public void minOfElements (View v){
-        displayFirstNumPlaceView = findViewById(R.id.firstNumber);
-        displaySecondNumPlaceView = findViewById(R.id.secondNumber);
-        displayResultPlaceView= findViewById(R.id.result);
-
-        int num1 = Integer.parseInt(displayFirstNumPlaceView.getText().toString());
-        int num2 = Integer.parseInt(displaySecondNumPlaceView.getText().toString());
         int res = num1 - num2;
-
         displayResultPlaceView.setText(Integer.toString(res));
     }
 
     public void mulOfElements (View v){
-        displayFirstNumPlaceView = findViewById(R.id.firstNumber);
-        displaySecondNumPlaceView = findViewById(R.id.secondNumber);
-        displayResultPlaceView= findViewById(R.id.result);
-
-        int num1 = Integer.parseInt(displayFirstNumPlaceView.getText().toString());
-        int num2 = Integer.parseInt(displaySecondNumPlaceView.getText().toString());
         int res = num1 * num2;
-
         displayResultPlaceView.setText(Integer.toString(res));
     }
 
     public void divOfElements (View v){
-        displayFirstNumPlaceView = findViewById(R.id.firstNumber);
-        displaySecondNumPlaceView = findViewById(R.id.secondNumber);
-        displayResultPlaceView= findViewById(R.id.result);
-
-        int num1 = Integer.parseInt(displayFirstNumPlaceView.getText().toString());
-        int num2 = Integer.parseInt(displaySecondNumPlaceView.getText().toString());
         int res = num1 / num2;
-
         displayResultPlaceView.setText(Integer.toString(res));
     }
 }
