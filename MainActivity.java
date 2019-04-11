41886b86df0348f9caf648610419c66fab8e3c04
NewAzgardec <46683133+NewAzgardec@users.noreply.github.com>
2019-04-11 04:37:51 +0300

    update

--- MainActivity.java
+++ MainActivity.java
@@ -8,15 +8,19 @@ import com.example.gradledemo.java.ConstantsFlavors;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "tag";
+    private static final String DEMO_VERSION = "demo";
+    private static final String FULL_VERSION = "full";
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if(ConstantsFlavors.type==ConstantsFlavors.Type.DEMO){
-            Log.i("TAG", "DEMO_VERSION");
-        }else {
-            Log.i("TAG", "FULL_VERSION");
+        if (ConstantsFlavors.type == ConstantsFlavors.Type.DEMO) {
+            Log.i(TAG, DEMO_VERSION);
+        } else {
+            Log.i(TAG, FULL_VERSION);
         }
     }
 }
